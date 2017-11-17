39f4ea807de14a704b7be2bcc14f81f11de9c3a5
Stephen Kelly <stevekm@users.noreply.github.com>
2017-11-17 17:50:06 -0500

    Update get_IGV.sh

--- bin/get_IGV.sh
+++ bin/get_IGV.sh
@@ -2,4 +2,4 @@
 
 # download IGV
 
-wget http://data.broadinstitute.org/igv/projects/downloads/IGV_2.3.81.zip && unzip IGV_2.3.81.zip
+wget http://data.broadinstitute.org/igv/projects/downloads/2.3/IGV_2.3.81.zip && unzip IGV_2.3.81.zip
